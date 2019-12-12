57319d1c8e5ed45ed9094abfec83e2351e027cec
Souriya Trinh <souriya.trinh+github@gmail.com>
2019-12-12 22:32:04 +0100

    Still trying to reproduce the bug.

---
+++ test/UseFaceDetection4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+#include "Simd/SimdDetection.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main(int argc, char * argv[])
+{
+    std::string filename = "";
+    for (int i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "--input" && i+1 < argc) {
+            filename = std::string(argv[i+1]);
+        }
+    }
+
+    int width = 0, height = 0, channels = 0;
+    unsigned char *image = stbi_load(filename.c_str(), &width, &height, &channels, STBI_grey);
+    if (image == NULL) {
+        std::cerr << "Cannot read: " << filename << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    //copy
+    std::vector<unsigned char> bitmap(width*height);
+    memcpy(bitmap.data(), image, width*height);
+
+    stbi_image_free(image);
+
+    std::cout << "width: " << width << " ; height: " << height << " ; channels: " << std::endl;
+
+    SimdSetThreadNumber(1);
+
+    typedef Simd::Detection<Simd::Allocator> Detection;
+    Detection detection;
+    if (detection.Load("haar_face_0.xml")) {
+        Detection::View image(width, height, Detection::View::Gray8, bitmap.data());
+        detection.Init(image.Size(), 1.1, Detection::Size(30,30));
+
+        Detection::Objects objects;
+        detection.Detect(image, objects);
+
+        for (size_t i = 0; i < objects.size(); i++) {
+            std::cout << i << ") " << objects[i].rect.Left() << ", " << objects[i].rect.Top() << " ; "
+                      << objects[i].rect.Width() << "x" << objects[i].rect.Height() << std::endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
